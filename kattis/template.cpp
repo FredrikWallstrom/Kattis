c933814ae39c78c18b998616b9df75319e5e9f4e
Fredrik <fredrik.wallstrom.1992@gmail.com>
2018-01-19 14:54:32 +0100

    Project created with a template.cpp file

---
+++ kattis/template.cpp
@@ -0,0 +1,32 @@
+/* -*- Mode: C++ -*- */
+
+/**
+ *
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <sstream>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+typedef vector<int> vi;
+
+int main() {
+
+
+    return 0;
+}
