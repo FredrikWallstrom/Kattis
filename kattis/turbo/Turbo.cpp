20f557559339a8044abcd1dba842542734637f31
Fredrik <fredrik.wallstrom.1992@gmail.com>
2018-01-29 11:26:52 +0100

    Started to work on Turbo

---
+++ kattis/turbo/Turbo.cpp
@@ -0,0 +1,46 @@
+/* -*- Mode: C++ -*- */
+
+/**
+ *  Author: Fredrik Wallström
+ *  Date:
+ *
+ *  Comments:
+ *
+ *  Lessons Learned:
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <sstream>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+typedef vector<int> vi;
+
+int main() {
+
+    int N, number;
+    scanf("%d", &N);
+    vector<int> numbers;
+
+    for (int i = 0; i < N; ++i) {
+        scanf("%d", &number);
+        numbers.push_back(number);
+    }
+
+
+    return 0;
+}
