59eff732f8bcfcaf33b1f62e74992eb32409df5b
Fredrik <fredrik.wallstrom.1992@gmail.com>
2018-02-08 11:11:34 +0100

    Modified fenwicktree so i can create more instances

--- kattis/fenwick_tree/Fenwick.cpp
+++ kattis/fenwick_tree/Fenwick.cpp
@@ -6,7 +6,7 @@
  *
  *  Comments: Fenwick tree.
  *
- *  Lessons Learned: 
+ *  Lessons Learned:
  */
 
 #include <algorithm>
@@ -28,56 +28,82 @@
 #include <iostream>
 
 using namespace std;
-typedef vector<int> vi;
 
-long long n,q;
-char ft;
-long long fenwickTree[5000001] ,delta;
 
-/**
- * Increases a[i] with the number delta.
- * Go forward in the bitTree and change all the values with delta.
- * The last syntax is to add the last set bit
- */
-void add(long long int i, long long int delta){
-    while(i <= n){
-        fenwickTree[i] += delta;
-        i += i & -i;
+
+class FenwickTree{
+
+private:
+    vector<long long int> tree;
+
+public:
+
+    /**
+     * Constructor to create an instance.
+     */
+    explicit FenwickTree(vector<long long int> vector) {
+        tree.resize(vector.size() + 1, 0);
+        buildTree(vector);
+    };
+
+    /**
+     * Initialize the tree.
+     */
+    void buildTree(vector<long long int> &vector) {
+        for (long long int i = 1; i <= vector.size(); ++i) {
+            add(i - 1, vector[i - 1]);
+        }
     }
-}
 
-/**
- * A query for the values of a[0] + a[1] + ... + a[i-1].
- * Go through backward in the bitTree and check sum all values at the way down.
- * The last syntax is to remove the last set bit
- */
-long long int number_sum(long long end){
-    long long int result = 0;
-    while(end > 0){
-        result += fenwickTree[end];
-        end -= end & -end;
+    /**
+    * Increases a[i] with the number delta.
+    * Go forward in the bitTree and change all the values with delta.
+    * The last syntax is to add the last set bit
+    */
+    void add(long long int i, long long int delta){
+        i++;
+        while(i <= tree.size()){
+            tree[i] += delta;
+            i += i & -i;
+        }
     }
-    return result;
-}
+
+    /**
+    * A query for the values of a[0] + a[1] + ... + a[i-1].
+    * Go through backward in the bitTree and check sum all values at the way down.
+    * The last syntax is to remove the last set bit
+    */
+    long long int number_sum(long long end){
+        long long int result = 0;
+        while(end > 0){
+            result += tree[end];
+            end -= end & -end;
+        }
+        return result;
+    }
+};
 
 /**
  * Main function that will read the input and then go through all queries, either call sum or add.
  */
 int main() {
     ios::sync_with_stdio(false);
-    long long int i;
+    long long int i, delta, n, q;
+    char ft;
 
     scanf("%lld", &n);
     scanf("%lld", &q);
-    
+
+    FenwickTree FT = FenwickTree(vector<long long int>(n));
+
     while(q > 0){
         scanf(" %c", &ft);
         scanf("%lld", &i);
         if(ft=='+'){
             scanf("%lld", &delta);
-            add(i+1, delta);
+            FT.add(i, delta);
         }else{
-            long long int res = number_sum(i);
+            long long int res = FT.number_sum(i);
             printf("%lld\n", res);
         }
         q--;
