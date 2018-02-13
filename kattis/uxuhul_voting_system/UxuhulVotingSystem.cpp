f16dce192e117496efeea3036ebcda9d6d4eac85
Fredrik <fredrik.wallstrom.1992@gmail.com>
2018-02-13 16:05:45 +0100

    Modified Uxuhul

--- kattis/uxuhul_voting_system/UxuhulVotingSystem.cpp
+++ kattis/uxuhul_voting_system/UxuhulVotingSystem.cpp
@@ -2,9 +2,11 @@
 
 /**
  *  Author: Fredrik Wallström
- *  Date:
+ *  Date: 13/2 -18
  *
- *  Comments:
+ *  Comments: Uxuhul, solved with DP. Idea is to calculate the outcome for the last priest, and for the second last
+ *              and so on. so reach the first priest.
+ *              In the matrix we got the result in [0][0] since we start with NNN wich represent the first column.
  *
  *  Lessons Learned:
  */
@@ -64,44 +66,61 @@ int main() {
 
         int result[nrOfVotingPriests][8];
 
-        // Fill result from last priest to first priest.
+        // Iterate to fill the results for the last priest.
+        // Go through all possible outComes.
+        for (int outCome = 0; outCome < 8; ++outCome) {
+            int bestOutcome = -1;
+            int bestPref = 10;
+            int currPref = 10;
+            int currInsideOutcome = -1;
+            // Go through the outcome, "inside" one outcome, check the flip counter array.
+            for (int insideOutcome = 0; insideOutcome < 8; ++insideOutcome) {
+                // If we are the last priest we cant access priest + 1.
+                // The last priest can flip if the "flip counter" is 1.
+                // Take out currentPreference for the last priest.
+                // Save the insideOutcome if the preference is the best.
+                if (flipCounter[outCome][insideOutcome] == 1) {
+                    currPref = preferences[nrOfVotingPriests - 1][insideOutcome];
+                    currInsideOutcome = insideOutcome;
+                }
+                if (currPref < bestPref) {
+                    bestOutcome = currInsideOutcome;
+                    bestPref = currPref;
+                }
+            }
+            // Set the result.
+            result[nrOfVotingPriests - 1][outCome] = bestOutcome;
+        }
+
+
+        // Iterate fill the results for every priest, NOT the last one.
         // Go through all priests, starting backwards.
-        for (int priest = nrOfVotingPriests - 1; priest >= 0; --priest) {
+        for (int priest = nrOfVotingPriests - 2; priest >= 0; --priest) {
             // Go through all possible outComes.
             for (int outCome = 0; outCome < 8; ++outCome) {
+
+                // We can break if we are on the first priest,
+                // And after we checked whats the outCome is for NNN.
+                if(priest == 0 && outCome == 1) break;
+
                 int bestOutcome = -1;
                 int bestPref = 10;
                 int currPref = 10;
                 int currInsideOutcome = -1;
-                // Go through the outcome "inside" one outcome, check the flip counter array.
+                // Go through the outcome, "inside" one outcome, check the flip counter array.
                 for (int insideOutcome = 0; insideOutcome < 8; ++insideOutcome) {
 
-                    // If we are the last priest we cant access priest + 1.
-                    // The last priest can flip if the "flip counter" is 1.
-                    // Take out currentPreference for the last priest.
-                    // Save the insideOutcome if the preference is the best.
-                    if(priest == nrOfVotingPriests - 1){
-                        if(flipCounter[outCome][insideOutcome] == 1){
-                            currPref = preferences[priest][insideOutcome];
-                            currInsideOutcome = insideOutcome;
-                        }
-
-                    // If we not are on the last priest, we check which outCome we can flip to.
                     // The current priest can flip if the "flip counter" is 1.
                     // Take out currentPreference for that priest, here we need to check in DP table for the priest "before".
                     // Save the insideOutcome if the preference is the best, which is given from priest "before".
-                    } else{
-                        if(flipCounter[outCome][insideOutcome] == 1) {
+                    if(flipCounter[outCome][insideOutcome] == 1) {
                             currPref = preferences[priest][result[priest + 1][insideOutcome]];
                             currInsideOutcome = result[priest + 1][insideOutcome];
-                        }
                     }
-
                     if (currPref < bestPref) {
                         bestOutcome = currInsideOutcome;
                         bestPref = currPref;
                     }
-
                 }
                 // Set the result.
                 result[priest][outCome] = bestOutcome;
